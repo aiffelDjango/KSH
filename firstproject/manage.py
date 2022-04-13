4c7f79c2c0b4c0a91083c0be8f61d2cfe70f58a8
kim-seo-hyun <zzz0000227@naver.com>
2022-04-13 17:29:49 +0900

    firstproject to config

--- firstproject/manage.py
+++ firstproject/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings') # 폴더 바꾸면 여길 바꿔야함 
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
